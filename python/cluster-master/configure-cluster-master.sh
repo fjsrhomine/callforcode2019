9b6551b0a5e26d00f3ac7a02da4dfcc2d4fc5f59
Francisco Santamaria <fjsr3000@hotmail.com>
2019-07-05 22:37:12 -0500

    adding the SSH-AGENT enable

--- python/cluster-master/configure-cluster-master.sh
+++ python/cluster-master/configure-cluster-master.sh
@@ -4,3 +4,7 @@
 
 #This script is to get the latest changes on the cluster-master folder
 sudo wget https://raw.githubusercontent.com/fjsrhomine/callforcode2019/master/python/cluster-master/cluster.py -O cluster.py
+
+#This enables the SSH-AGENT to allow the connectivity without typing the password everytime
+eval $(ssh-agent) 
+ssh-add ~/.ssh/id_rsa_rojo 
