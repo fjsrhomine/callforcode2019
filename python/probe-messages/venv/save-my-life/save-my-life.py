eb916faa3e7f762f43979b3560f568f8d0709dbb
Francisco Santamaria <fjsr3000@hotmail.com>
2019-07-11 21:53:36 -0500

    test

--- python/probe-messages/venv/save-my-life/save-my-life.py
+++ python/probe-messages/venv/save-my-life/save-my-life.py
@@ -32,7 +32,7 @@ iface = "wlan0"
 sender="b8:27:eb:be:a8:12"
 payload="Cosa extremadamanete grande para poder detectarla"
 
-dot11 = Dot11(type=0, subtype=4, addr1=sender, addr2="ff:ff:ff:ff:ff:ff", addr3=sender)
+dot11 = Dot11(type=0, subtype=4, addr1="ff:ff:ff:ff:ff:ff", addr2=sender, addr3=sender)
 probe =Dot11ProbeReq()
 essid=Dot11Elt(ID='SSID', info=SSID, len=len(SSID))
 
