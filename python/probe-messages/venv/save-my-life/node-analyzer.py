60bc9e3a137d02971c988a0c84ab20db4af88df2
Francisco Santamaria <fjsr3000@hotmail.com>
2019-07-12 00:58:55 -0500

    Determinacion de NodeAnalyzer version 1

--- python/probe-messages/venv/save-my-life/node-analyzer.py
+++ python/probe-messages/venv/save-my-life/node-analyzer.py
@@ -10,7 +10,7 @@ for packet in scapy_cap:
         print("<Probe_found>")
         print("time = " + str(packet.time)) # time
         print("mac = " + packet.addr2[:32]) # mac
-        print("SC = " + str(packet[Dot11].sc))  # SC
+        print("SC = " + str(packet[Dot11].SC))  # SC - Sequence Control
         # if packet[Dot11][Dot11ProbeReq].haslayer(Dot11EltVendorSpecific):
             # packet[Dot11][Dot11ProbeReq][Dot11Elt][Dot11EltVendorSpecific].show()
             # print("SSID = " + binascii.b2a_base64(packet[Dot11][Dot11ProbeReq][Dot11Elt][Dot11EltVendorSpecific].info).decode('utf-8')[:32])
