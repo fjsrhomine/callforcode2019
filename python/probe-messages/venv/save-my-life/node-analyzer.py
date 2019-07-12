2a77c60f914b9426bbf14dd36ae8e76b91c4e1ea
Francisco Santamaria <fjsr3000@hotmail.com>
2019-07-11 23:00:48 -0500

    ters

--- python/probe-messages/venv/save-my-life/node-analyzer.py
+++ python/probe-messages/venv/save-my-life/node-analyzer.py
@@ -2,5 +2,7 @@ from scapy.all import *
 
 scapy_cap = rdpcap('/tmp/tshark-temp_00001_20190712035555')
 for packet in scapy_cap:
-    packet.haslayer(Dot11ProbeReq)
-    print (packet[IPv6].src)
+    if packet.haslayer(Dot11ProbeReq):
+        packet.show()
+    else:
+        print ("No Probe")
