ed50a468d051c457f6569a6df8637a20b17ceffb
Francisco Santamaria <fjsr3000@hotmail.com>
2019-07-11 22:56:57 -0500

    test

--- python/probe-messages/venv/save-my-life/node-analyzer.py
+++ python/probe-messages/venv/save-my-life/node-analyzer.py
@@ -1,6 +1,6 @@
 from scapy.all import *
 
-scapy_cap = rdpcap('/tmp/tshark-temp_00006_20190712031104')
+scapy_cap = rdpcap('/tmp/tshark-temp_00001_20190712035555')
 for packet in scapy_cap:
     packet.haslayer(Dot11ProbeReq)
     print (packet[IPv6].src)
