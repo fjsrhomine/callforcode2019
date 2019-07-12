9741fc4cb85c70f6f5256ebbd9618bc54bb56977
Francisco Santamaria <fjsr3000@hotmail.com>
2019-07-12 07:48:20 -0500

    adding mac filter

--- python/probe-messages/venv/save-my-life/node-analyzer.py
+++ python/probe-messages/venv/save-my-life/node-analyzer.py
@@ -5,21 +5,24 @@ from scapy.all import *
 
 def analyzeSaveMyLife(packet):
     if packet.haslayer(Dot11ProbeReq):
-        # packet.show()
-        print("<Probe_found>")
-        print("\ttime = " + str(packet.time))  # time
-        print("\tmac = " + packet.addr2[:32])  # mac
-        print("\tSC = " + str(packet[Dot11].SC))  # SC - Sequence Control
-        print("</Probe_found>")
+        if( filterThisMac =="" or packet.addr2[:32] == filterThisMac):
+            # packet.show()
+            print("<Probe_found>")
+            print("\ttime = " + str(packet.time))  # time
+            print("\tmac = " + packet.addr2[:32])  # mac
+            print("\tSC = " + str(packet[Dot11].SC))  # SC - Sequence Control
+            print("\tFullPacket")
+            packet.show()
+            print("</Probe_found>")
 
 
 print("!! Homine-Unnks presents: save-my-life  protocol example")
 
-# Getting the file name to work on
-filename = input('Enter the path/fileName to work on: ')
-
 # Getting the interface
 iface = input('Enter the wifi interface (wlan1 for example): ')
+
+# Getting the mac to filter
+filterThisMac = input('Enter a MAC for a device to monitor (optional): ')
 
 # sniffing in real time the content of the file
 sniff(iface=iface, prn=analyzeSaveMyLife)
