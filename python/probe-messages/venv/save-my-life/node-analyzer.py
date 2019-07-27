b56ebf6a6ee4699fc12584534d4323670ae8af4d
Francisco Santamaria <fjsr3000@hotmail.com>
2019-07-26 22:16:29 -0500

    testing

--- python/probe-messages/venv/save-my-life/node-analyzer.py
+++ python/probe-messages/venv/save-my-life/node-analyzer.py
@@ -26,7 +26,8 @@ def analyzeSaveMyLife(packet):
                 print("\ttime = " + str(packet.time))  # time
                 print("\tmac = " + packet.addr2[:32])  # mac
                 print("\tSC = " + str(packet[Dot11].SC))  # SC - Sequence Control
-                print("\tSC = " + -(256-ord(packet.notdecoded[-3:-2])))  # Signal Strength
+                print("\tNotDecoded = " + packet.notdecoded)  # Signal Strength
+                #print("\tSignal Strength = " + -(256-ord(packet.notdecoded[-3:-2])))  # Signal Strength
                 print("\tFullPacket")
                 print("</Probe_found>")
                 # packet.show()
